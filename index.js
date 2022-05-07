1e62f728defa6d073ae5bf557ab79418751daf27
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-05-07 23:41:13 +0600

    added mongo and Get request

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@gpu-point.o7ldd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const startServe = async () =>{
+    try{
+        await client.connect();
+        const productCollections = client.db('gpu-point').collection('products');
+
+        app.get("/all-products", async (req,res) =>{
+            const cursor = productCollections.find({});
+            const products = await cursor.toArray();
+            res.status(200).send(products);
+        })
+        
+
+        
+    }
+    finally{
+
+    }
+
+}
+
+startServe().catch(console.dir);
+
+
 app.get("/", (req,res) =>{
     res.json({
-        mgs : "hellow",
+        mgs : "hellow Im working",
     })
 })
 
